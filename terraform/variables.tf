fabe18fab9730c278dbf56bfae6c91678f7e1976
JoanKr <asik103mt@gmail.com>
2025-06-03 15:45:35 +0200

    add version

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,7 +5,7 @@ variable "region" {
 }
 
 variable "clusterName" {
-  description = "Name of the EKS cluster name"
+  description = "Name of the EKS cluster"
   type        = string
   default     = "vprofile-eks"
 }
